61c61703ef56c3a31a724bda064925bc40eb6f31
cgf2172 <cgf2172@gmail.com>
2023-03-31 20:33:10 +0200

    31/3 ultimo

--- src/com/hedima/presentacion/nota.java
+++ src/com/hedima/presentacion/nota.java
@@ -5,23 +5,42 @@ import java.util.Scanner;
 public class nota {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese la nota: ");
-        int nota = teclado.nextInt();
-        switch (nota) {
-            case 10,9:
-                System.out.println("Sobresaliente");
+        //System.out.println("Ingrese la nota: ");
+       // int nota = teclado.nextInt();
+        //switch (nota) {
+           // case 10,9:
+              //  System.out.println("Sobresaliente");
+               // break;
+           // case 8,7:
+              //  System.out.println("Notable");
+               // break;
+         //   case 6,5:
+             //   System.out.println("Aprobado");
+             //   break;
+          //  case 4,3,2,1,0:
+            //    System.out.println("Rechazado");
+            //    break;
+           // default:
+             //   System.out.println("Nota invalida");
+       // }
+        System.out.println("Ingrese pais: ");
+        String pais = teclado.next();
+        pais= pais.toLowerCase();
+        switch (pais){
+            case "argentina":
+                System.out.println("Argento");
                 break;
-            case 8,7:
-                System.out.println("Notable");
+            case "brasil":
+                System.out.println("Brasilero");
                 break;
-            case 6,5:
-                System.out.println("Aprobado");
+            case "colombia":
+                System.out.println("Colombiano");
                 break;
-            case 4,3,2,1,0:
-                System.out.println("Rechazado");
+            case "peru":
+                System.out.println("Peruano");
                 break;
             default:
-                System.out.println("Nota invalida");
+                System.out.println("de otro país");
         }
     }
 }
